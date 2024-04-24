f73249e5ead5337fe61bdb1ec2d2000ac43e3592
ksingh7 <ksingh7@uiowa.edu>
2024-04-24 17:58:34 -0400

    change logo

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { Text, useColorModeValue } from '@chakra-ui/react'
-import FootprintIcon from './icons/footprint'
+import CoinIcon from './icons/coin'
 import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
@@ -26,7 +26,7 @@ const Logo = () => {
     (<Link href="/" scroll={false}>
 
       <LogoBox>
-        <FootprintIcon />
+        <CoinIcon />
         <Text
           color={useColorModeValue('gray.800', 'whiteAlpha.900')}
           fontFamily='M PLUS Rounded 1c", sans-serif'
